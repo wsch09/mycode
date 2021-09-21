72e873bb47b0fcdaaa656b92fca2fda621fc7c05
Bill <ilmka3@gmail.com>
2021-09-21 21:11:53 +0000

    panic

---
+++ panic01.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    // panic produces a quick exit
+    panic("Jim, we have a problem.")
+
+    fmt.Println("You will not even see this line. The panic creates a fast fail.")
+}
+
